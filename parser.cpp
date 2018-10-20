6c0c500d15bd6d0595aadb2a46cfe4cac7162915
taylor-santos <taylor.p.santos@gmail.com>
2018-10-19 18:52:26 -0700

    Modified reflex-bison-ast to parse Quack (.qk) files

--- parser.cpp
+++ parser.cpp
@@ -4,49 +4,29 @@
 //
 
 #include "lex.yy.h"
-#include "ASTNode.h"
 #include "EvalContext.h"
 #include "Messages.h"
 
 class Driver {
 public:
-    explicit Driver(reflex::Input in) : lexer(in), parser(new yy::parser(lexer, &root))
-       { root = nullptr; }
+    explicit Driver(reflex::Input in) : lexer(in), parser(new yy::parser(lexer)){}
     ~Driver() { delete parser; }
-    AST::ASTNode* parse() {
-        // parser->set_debug_level(1); // 0 = no debugging, 1 = full tracing
-        // std::cout << "Running parser\n";
+    bool parse() {
         int result = parser->parse();
-        if (result == 0 && report::ok()) {  // 0 == success, 1 == failure
-            // std::cout << "Extracting result\n";
-            if (root == nullptr) {
-                std::cout << "But I got a null result!  How?!\n";
-            }
-            return root;
-        } else {
-            std::cout << "Parse failed, no tree\n";
-            return nullptr;
-        }
+        return result == 0 && report::ok();
     }
 private:
     yy::Lexer   lexer;
     yy::parser *parser;
-    AST::ASTNode *root;
 };
 
 int main()
 {
     Driver driver(std::cin);
-    AST::ASTNode* root = driver.parse();
-    if (root != nullptr) {
-        std::cout << "Parsed!\n";
-        AST::AST_print_context context;
-        root->json(std::cout, context);
-        std::cout << std::endl;
-        // std::cout << "Successfully parsed " << root->str() << std::endl;
-        auto ctx = EvalContext();
-        std::cout << "Evaluates to " << root->eval(ctx) << std::endl;
+    bool parse_result = driver.parse();
+    if (parse_result) {
+        std::cout << "Finished parse with no errors" << std::endl;
     } else {
-        std::cout << "Extracted root was nullptr" << std::endl;
+        std::cout << "Unable to parse!" << std::endl;
     }
 }
